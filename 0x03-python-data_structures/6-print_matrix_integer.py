3b77228c5c01304ffe64d5668f7bd934cf1c1941
Kipyegon Rotich <danyego100@gmail.com>
2022-09-27 17:43:27 -0700

    Data strauctures in python

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-
-
 def print_matrix_integer(matrix=[[]]):
-    """Prints a matrix of ints"""
-    for row in matrix:
-        for i in row:
-            if i != row[-1]:
-                print("{:d}".format(i), end="")
+    n = 0
+    m = 0
+    for i in matrix:
+        m = 0
+        for j in i:
+            if m < len(i)-1:
+                print("{:d}".format(matrix[n][m]), end=" ")
             else:
-                print("{:d}".format(i), end="")
+                print("{:d}".format(matrix[n][m]), end="")
+            m += 1
         print()
+        n += 1
