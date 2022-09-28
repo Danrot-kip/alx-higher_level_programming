6f04a04131887c7f4dfe0e8b38af330a8f475d35
Kipyegon Rotich <danyego100@gmail.com>
2022-09-27 17:48:05 -0700

    Data strauctures in python

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,53 +1,33 @@
 #include "lists.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
-* list_len - finds no. of elements ina linked list.
-* @h: pointer to linked list.
-*
-* Return: number of elements in linked list.
-*/
-size_t list_len(listint_t *h)
-{
-	size_t  nodes = 0;
-
-	if (h == NULL)
-		return (0);
-	while (h != NULL)
-	{
-		nodes++;
-		h = h->next;
-	}
-	return (nodes);
-}
-
-/**
-* is_palindrome - checks if a singly linked list is a palindrome.
-* @head: double pointert to head of d-list.
-*
-* Return: 1 if palindrome, 0 otherwise.
-*/
+ * is_palindrome - tell if list is palindrome or not
+ * @head: pointer to list to be freed
+ * Return: int
+ */
 int is_palindrome(listint_t **head)
 {
-	int *nArr, i = 0, j = 0, len = 0;
+	int array[4096], cont0 = 0, cont1 = 0;
 	listint_t *temp;
 
-	if (*head == NULL)
+	if (head == NULL)
+		return (0);
+
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	temp = *head;
-	len = list_len(temp);
-	nArr = (int *)malloc(sizeof(int) * len);
-	if (nArr == NULL)
-		return (2);
+
 	temp = *head;
 	while (temp != NULL)
 	{
-		nArr[j] = temp->n;
-		j++;
+		array[cont1] = temp->n;
 		temp = temp->next;
+		cont1++;
 	}
-	for (i = 0, j = len - 1; i < j; i++, j--)
+	cont1--;
+	for ( ; cont0 < cont1 ; cont0++, cont1--)
 	{
-		if (nArr[i] != nArr[j])
+		if (array[cont0] != array[cont1])
 			return (0);
 	}
 	return (1);
