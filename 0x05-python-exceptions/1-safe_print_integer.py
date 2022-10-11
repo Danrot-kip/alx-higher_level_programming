4c9febf945fad2116a513bad377be2b011a3fb28
Kipyegon Rotich <danyego100@gmail.com>
2022-10-11 13:37:21 -0700

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-
-
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except:
+        return True
+    except TypeError:
         return False
-    return True
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except TypeError:
+        return False
+    except ValueError:
+        return False
